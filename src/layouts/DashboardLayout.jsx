8d4f162beb269efadbb2c516bfeab94aae09e057
pimp13 <pimp.puma.13@gmail.com>
2026-08-10 15:43:46 +0330

    fix sidebar and refactoring dashboard page

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,24 +1,23 @@
 import { Outlet } from "react-router-dom";
 
 export default function DashboardLayout() {
+  // <section className="mt-2 bg-stone-100">
+  //   <header className="flex items-center bg-stone-200/20 justify-between mx-4 h-10 shadow-sm rounded-2xl px-4">
+  //     <nav className="flex items-center gap-12">
+  //         <Link to={'/'}>
+  //           خانه
+  //         </Link>
+  //     </nav>
+  //     <Link to="/">#</Link>
+  //   </header>
+
+  //   <main className="mt-2">
+  //     <Outlet /> {/* محتوای صفحات اینجا رندر می‌شود */}
+  //   </main>
+
+  //   <footer>فوتر سایت</footer>
+  // </section>
   return (
-    // <section className="mt-2 bg-stone-100">
-    //   <header className="flex items-center bg-stone-200/20 justify-between mx-4 h-10 shadow-sm rounded-2xl px-4">
-    //     <nav className="flex items-center gap-12">
-    //         <Link to={'/'}>
-    //           خانه
-    //         </Link>
-    //     </nav>
-    //     <Link to="/">#</Link>
-    //   </header>
-
-    //   <main className="mt-2">
-    //     <Outlet /> {/* محتوای صفحات اینجا رندر می‌شود */}
-    //   </main>
-
-    //   <footer>فوتر سایت</footer>
-    // </section>
-
     <section className="bg-[#F5F8FD]">
       <Outlet />
       {/* <footer className="bg-neutral-200 p-10">Footer</footer> */}
